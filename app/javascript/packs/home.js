00825f32c9264b89f9f6585f81950ddfb89db944
Colin Mink <minkcolin@gmail.com>
2021-05-02 22:34:40 -0700

    check for the time between work orders and add a basic alert

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,9 +1,61 @@
 const row_height = 100;
 
 document.body.onload = function() {
+    document.getElementById("grid-container").addEventListener("click",timeBetweenWorkOrders);
     setWorkOrderSizeAndPlacement();
 }
 
+
+function timeBetweenWorkOrders(event){
+    if(event.path.find(element => element.classList ? element.classList.contains("work-order") : false )){
+        alert("Work Order in progress");
+        console.log("Clicked a work order");
+    } else {
+        console.log("previousWorkOrder");
+        let previousWorkOrder = checkPreviousSiblingHasChild(event.target);
+        let previousOrderTime = null;
+        if(previousWorkOrder !== null){
+            previousOrderTime = new Date(previousWorkOrder.children[0].dataset.time * 1000);
+            console.log(previousOrderTime);
+        }
+        console.log("nextWorkOrder");
+        let nextWorkOrder = checkNextSiblingHasChild(event.target);
+        let nextOrderTime = null;
+        if(nextWorkOrder !== null){
+            nextOrderTime = new Date(nextWorkOrder.children[0].dataset.time * 1000);
+            console.log(nextOrderTime);
+        }
+        if(previousWorkOrder === null){
+            alert("First work order of the day at: " + nextOrderTime);
+        } else if(nextWorkOrder === null){
+            alert("No further work orders");
+        } else {
+            alert(msToTime(nextOrderTime - previousOrderTime) + " until the next work order");
+        }
+    }
+}
+//takes a dom elelemt and rescursively finds the previous sibiling that has a work-order class
+function checkPreviousSiblingHasChild(dom) {
+    if(dom.previousElementSibling === null) {
+        return null;
+    }
+    if(dom.previousElementSibling.children[0] !== null && dom.previousElementSibling.children[0] !== undefined){
+        return dom.previousElementSibling;
+    } else {
+        return checkPreviousSiblingHasChild(dom.previousElementSibling);
+    }
+}
+
+function checkNextSiblingHasChild(dom) {
+    if(dom.nextElementSibling === null) {
+        return null;
+    }
+    if(dom.nextElementSibling.children[0] !== null && dom.nextElementSibling.children[0] !== undefined){
+        return dom.nextElementSibling;
+    } else {
+        return checkNextSiblingHasChild(dom.nextElementSibling);
+    }
+}
 function setWorkOrderSizeAndPlacement() {
     let work_orders = document.getElementsByClassName("work-order");
     for (work_order of work_orders) {
@@ -18,3 +70,12 @@ function setWorkOrderSizeAndPlacement() {
     }
 
 }
+
+function msToTime(duration) {
+    var milliseconds = parseInt((duration % 1000) / 100),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    return hours + ":" + minutes;
+  }
